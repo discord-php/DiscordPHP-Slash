ed3d7685e9fd57228204c5a6fb85f4759134c1ff
Katie Volz <iggyvolz@gmail.com>
2021-09-08 23:28:27 -0400

    Fix deprecation warning using type declaration

--- src/Discord/Parts/Part.php
+++ src/Discord/Parts/Part.php
@@ -80,8 +80,7 @@ class Part implements JsonSerializable
      *
      * @return array
      */
-    #[\ReturnTypeWillChange]
-    public function jsonSerialize()
+    public function jsonSerialize(): array
     {
         return $this->attributes;
     }
